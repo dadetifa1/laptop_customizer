98ba99d2e3ec5c01ef0a00c722b853129a1a243d
Dele <dadetifa1@yahoo.com>
2020-11-16 10:05:11 -0600

    inital load

---
+++ src/composition/Summary.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Total from './Total'
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+class Summary extends React.Component{
+    render (){
+        const summary = Object.keys(this.props.selected).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const selectedOption = this.props.selected[feature];
+      
+            return (
+              <div className="summary__option" key={featureHash}>
+                <div className="summary__option__label">{feature} </div>
+                <div className="summary__option__value">{selectedOption.name}</div>
+                <div className="summary__option__cost">
+                  {USCurrencyFormat.format(selectedOption.cost)}
+                </div>
+              </div>
+            );
+        });
+
+        return(
+            <>
+                <h2>Your cart</h2>
+                {summary}
+                <Total selected={this.props.selected}/>
+            </>
+        );
+    }
+}
+
+export default Summary;
