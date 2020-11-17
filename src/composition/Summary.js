3cbc0f50e71792c4209dff072a18f060822e9fb1
Dele <dadetifa1@yahoo.com>
2020-11-16 21:10:44 -0600

    recommended chaanges

--- src/composition/Summary.js
+++ src/composition/Summary.js
@@ -1,32 +1,14 @@
 import React from 'react'
 import Total from './Total'
+import SummaryItem from './SummaryItem'
 
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-});
 
 class Summary extends React.Component{
     render (){
-        const summary = Object.keys(this.props.selected).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
-            const selectedOption = this.props.selected[feature];
-      
-            return (
-              <div className="summary__option" key={featureHash}>
-                <div className="summary__option__label">{feature} </div>
-                <div className="summary__option__value">{selectedOption.name}</div>
-                <div className="summary__option__cost">
-                  {USCurrencyFormat.format(selectedOption.cost)}
-                </div>
-              </div>
-            );
-        });
-
         return(
             <>
                 <h2>Your cart</h2>
-                {summary}
+                <SummaryItem selected={this.props.selected}></SummaryItem>
                 <Total selected={this.props.selected}/>
             </>
         );
